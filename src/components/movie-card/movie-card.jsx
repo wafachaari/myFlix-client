f620c9ec945b4c4920f4595624cba63df8062809
wafachaari <wafachaari30@gmail.com>
2021-04-21 14:35:44 +0200

    Update movie-card.jsx
    movie-card updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
  
   //movie in const { movie } = this.props is the name of the prop used in <MovieCard … />.
   import React from 'react';
+  import PropTypes from 'prop-types';
 
   export class MovieCard extends React.Component {
    
@@ -11,11 +12,28 @@
   }
   }
 
+  
   MovieCard.propTypes = {
     movie: PropTypes.shape({
       Title: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired,
-      ImagePath: PropTypes.string.isRequired
+      ImagePath: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name:PropTypes.string.isRequired,
+        Description:PropTypes.string.isRequired,
+
+      }),
+      Director:PropTypes.shape({
+        Name:PropTypes.string.isRequired,
+
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string,
+    }),
+
+
+
+
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
   };
