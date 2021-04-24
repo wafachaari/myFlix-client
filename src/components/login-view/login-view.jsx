1c35156f4bbfcb3fc02b83a7d5a64d7b34db133f
wafachaari <wafachaari30@gmail.com>
2021-04-24 18:38:36 +0200

    Update login-view.jsx
    login-view updated

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
 import PropTypes from 'prop-types';
+ import { Form, Button } from 'react-bootstrap';
+ import './login-view.scss';
+ import { Link } from 'react-router-dom';
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
@@ -14,17 +18,32 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container className="login-view" fluid="true">
+      <Form>
+        <Form.Group controlId="formBasicText">
+         <Form.Label>username</Form.Label>
+         <Form.Control type='text' 
+         placeholder="Enter username"       
+         value={username} 
+         onChange={e => setUsername(e.target.value)} />
+         
+      </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+             <Form.Label>Password</Form.Label>
+             <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+      </Form.Group>
+      <Button className="submit-button" variant="primary" type="submit">Submit</Button>
+      </Form>
+
+
+      <Form.Group>
+ <Form.Text className="text-muted"> don't have an account?</Form.Text>
+   
+    <Button className="login-button" >register</Button>
+    
+ </Form.Group>
+    </Container>
+     
   );
 }
 
