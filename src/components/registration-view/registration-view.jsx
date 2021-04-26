a8da2cdaf360765d646e18ca751a946e4eb53227
wafachaari <wafachaari30@gmail.com>
2021-04-26 13:02:54 +0200

    Update registration-view.jsx
    registration-view updated

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -13,8 +13,24 @@ export function RegistrationView(props){
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    /*axios.post('https://movie-api-db-30.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+    })
+    .catch(e => {
+      console.log('error registering the user')
+    });
+*/
     console.log(username, password, email, birthday);
-    props.onRegister();
+   props.onRegister();
   };
  
   return (
