3bb5e566b9eebac2ea5c83107729bd7e4e3a8955
wafachaari <wafachaari30@gmail.com>
2021-04-26 13:02:43 +0200

    Update login-view.jsx
    updatedlogin-view updated

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,24 +2,32 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import PropTypes from 'prop-types';
  import { Form, Button } from 'react-bootstrap';
+ import axios from 'axios';
+ import { BrowserRouter as Router, Route } from "react-router-dom";
  import './login-view.scss';
  import { Link } from 'react-router-dom';
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e) => { 
     e.preventDefault();
-    console.log(username, password);
-    //props.onLoggedIn(username)
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-     props.onLoggedIn(username);
+      axios.post('https://movie-api-db-30.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('no such user')
+    });
   };
 
   return (
     <Container className="login-view" fluid="true">
-      <Form>
+      <Form  >
         <Form.Group controlId="formBasicText">
          <Form.Label>username</Form.Label>
          <Form.Control type='text' 
@@ -32,7 +40,7 @@ export function LoginView(props) {
              <Form.Label>Password</Form.Label>
              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
       </Form.Group>
-      <Button className="submit-button" variant="primary" type="submit">Submit</Button>
+      <Button className="submit-button" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
       </Form>
 
 
