77c2e80f74cfe5f3b047657f51cf788ede80557a
wafachaari <wafachaari30@gmail.com>
2021-05-03 12:03:53 +0200

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,25 +5,33 @@ import PropTypes from 'prop-types';
  import axios from 'axios';
  import { BrowserRouter as Router, Route } from "react-router-dom";
  import './login-view.scss';
+ 
+import { RegistrationView } from '../registration-view/registration-view';
  import { Link } from 'react-router-dom';
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
-
+// a POST request is made to the login endpoint of the myFlix API using Axios.
+//the backend sends back the token and username
+// update the user state so that the main view is rendered again and, 
+// save authentication data in localStorage 
   const handleSubmit = (e) => { 
     e.preventDefault();
+    console.log('topM');
       axios.post('https://movie-api-db-30.herokuapp.com/login', {
       Username: username,
       Password: password
     })
     .then(response => {
       const data = response.data;
+      console.log(data,'typeData');
       props.onLoggedIn(data);
     })
     .catch(e => {
       console.log('no such user')
     });
   };
+   
 
   return (
     <Container className="login-view" fluid="true">
@@ -38,18 +46,18 @@ export function LoginView(props) {
       </Form.Group>
       <Form.Group controlId="formBasicPassword">
              <Form.Label>Password</Form.Label>
-             <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+             <Form.Control type="password" placeholder="Password" value={password}  onChange={e => setPassword(e.target.value)}/>
       </Form.Group>
       <Button className="submit-button" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
       </Form>
 
 
-      <Form.Group>
- <Form.Text className="text-muted"> don't have an account?</Form.Text>
-   
-    <Button className="login-button" >register</Button>
-    
- </Form.Group>
+      <Form.Group className="registration-group" controlId="formRegistration">
+          <Form.Text className="text-muted">Don't have an account?</Form.Text>
+          <Link to={`/register`}>
+            <Button className="register-link">Register here</Button>
+          </Link>
+        </Form.Group>
     </Container>
      
   );
