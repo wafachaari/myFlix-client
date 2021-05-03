fae56cd3f1b79e66d6d1e911fc259bc3bd6cf9fa
wafachaari <wafachaari30@gmail.com>
2021-05-04 00:39:45 +0200

    Update genre-view.jsx
    genre-view updated

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-//import ListGroup from 'react-bootstrap/ListGroup';
-//import './genre-view.scss';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './genre-view.scss';
 import { Link } from 'react-router-dom';
 
 export class GenreView extends React.Component {
@@ -17,11 +17,15 @@ export class GenreView extends React.Component {
 
   render() {
     const { genre } = this.props;
+
+
+    if (!genre) return null;
+
     return (
       <Card className = "genre-view" style={{ width: '32rem' }}>
         <Card.Body>
-          <Card.Title className = "genre-name">{Genre.Name}</Card.Title>
-          <Card.Text className = "genre-description">{Genre.Description}</Card.Text>
+          <Card.Title className = "genre-name">{genre.Name}</Card.Title>
+          <Card.Text className = "genre-description">{genre.Description}</Card.Text>
           <Link to = {`/`}>
             <Button className="back-button">Back</Button>
           </Link>
