613124ae1e4159d594e7d35f2ff4f9898cb4f570
wafachaari <wafachaari30@gmail.com>
2021-05-03 12:03:27 +0200

    Create director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'; 
+ 
+import { Link } from 'react-router-dom';
+export class DirectorView extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+
+  render() {
+   
+      const { director } = this.props;
+    return (
+      <Card className = "director-view" style={{ width: '32rem' }}>
+       
+        <Card.Body>
+          <Card.Title className = "director-name">{Director.Name}</Card.Title>
+          <Card.Text className = "director-bio">{Birector.Bio}</Card.Text>
+          <Card.Title className = "director-birth">{Director.Birth}</Card.Title>
+          <Card.Text className = "director-Death">{Birector.Death}</Card.Text>
+          
+          <Link to = {`/`}>
+            <Button className="back-button">Back</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+
+
+  }}
+  DirectorView.propTypes = {
+    director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string.isRequired,
+    }).isRequired
+  };
