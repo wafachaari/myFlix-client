cb2a60bdcc8c1b6a366cb4eee6188eae7e506e36
wafachaari <wafachaari30@gmail.com>
2021-05-06 15:14:08 +0200

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,52 +1,56 @@
- 
-  //movie in const { movie } = this.props is the name of the prop used in <MovieCard … />.
-  import React from 'react';
-  import PropTypes from 'prop-types';
-  import Button from 'react-bootstrap/Button';
+
+//movie in const { movie } = this.props is the name of the prop used in <MovieCard … />.
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
 import { Link } from 'react-router-dom';
 
-  export class MovieCard extends React.Component {
-   
+export class MovieCard extends React.Component {
+
   render() {
     const { movie } = this.props;
-return(
-  <Card className="card"  style={{ width:'200'}}>
-    
-    <Card.Img variant="top" src={movie.ImagePath} />
-     <Card.Body>
-  <Card.Title className="movie-title">{movie.Title}</Card.Title>
-  <Card.Text>{movie.Description}</Card.Text> 
-  <Link to={`/movies/${movie._id}`}>
-            <Button variant="link">Open</Button>
-          </Link>
-          
-</Card.Body>
-  </Card>
+    return (
+      <Card className="card" style={{ width: '200' }}>
 
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title className="movie-title">{movie.Title}</Card.Title>
+          {/* <Card.Text>{movie.Description}</Card.Text> */}
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Movie Details</Button>
+          </Link>
+
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button variant="link">  Director:  <span className="directorname">  {movie.Director.Name}</span></Button>
+          </Link>
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">  Genre: <span className="directorname"> {movie.Genre.Name}</span></Button>
+          </Link>
+        </Card.Body>
+      </Card>
     );
   }
 }
- 
-  MovieCard.propTypes = {
-    movie: PropTypes.shape({
-      Title: PropTypes.string.isRequired,
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired,
-      ImagePath: PropTypes.string.isRequired,
-      Genre: PropTypes.shape({
-        Name:PropTypes.string.isRequired,
-        Description:PropTypes.string.isRequired,
 
-      }),
-      Director:PropTypes.shape({
-        Name:PropTypes.string.isRequired,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
 
       Bio: PropTypes.string.isRequired,
       Birth: PropTypes.string.isRequired,
       Death: PropTypes.string,
     }),
 
-    }).isRequired,
-/*onMovieClick: PropTypes.func.isRequired*/
-  };
+  }).isRequired,
+  /*onMovieClick: PropTypes.func.isRequired*/
+};
