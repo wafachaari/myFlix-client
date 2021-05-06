1727b6cdf218b04472a8e47e438ed1c2ff064e2f
wafachaari <wafachaari30@gmail.com>
2021-05-06 16:19:17 +0200

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
-export const SET_BUTTON = 'SET_BUTTON';
+ 
 export const SET_FAVORITE = 'SET_FAVORITE';
 
 export function setMovies(value) {
@@ -17,9 +17,7 @@ export function setUser(value) {
   return { type: SET_USER, value };
 }
 
-export function setButton(value) {
-  return { type: SET_BUTTON, value };
-}
+ 
 
 export function setFavorite(value) {
   return { type: SET_FAVORITE, value };
