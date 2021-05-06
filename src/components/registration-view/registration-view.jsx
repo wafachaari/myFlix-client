8ffd4e90b98f701258b9ef696bed955a54065ed1
wafachaari <wafachaari30@gmail.com>
2021-05-06 15:13:53 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,117 +1,108 @@
- 
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import { Form, Button } from 'react-bootstrap';
-import './registration-view.scss';
+import React, { useState } from 'react';
+import './registration-view.scss'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
- 
-import { Link } from 'react-router-dom'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-export function RegistrationView(props){
+export function RegistrationView() { 
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
-
+  const [validated, setValidated] = useState('');
+  const [used, setUsed] = useState('');
 
   const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      setUsed(null);
+      setValidated(true);
+      return;
+    }
     e.preventDefault();
-
+    console.log(name, username);
+    /* Send a request to the server for authentication */
     axios.post('https://movie-api-db-30.herokuapp.com/users', {
+      
       Username: username,
       Password: password,
       Email: email,
       Birthday: birthday
     })
-    .then(response => {
-      const data = response.data;
-      console.log(data);
-      window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
-    })
-    .catch(e => {
-      console.log('error registering the user')
-    });
+      .then(response => {
+        if (response.status === 200) {
+          setUsed(true);
+        }
+        else {
+          window.open('/', '_self');
+        }
 
-    //console.log(username, password, email, birthday);
-   //props.onRegister();
+      })
+      .catch(e => {
+        console.log(e);
+      });
   };
- 
+
+  const setUsernameAndUsed = (e) => {
+    setUsername(e.target.value);
+    setUsed(null);
+  }
+
+
   return (
     <Container className="registration-view" fluid="true">
       <h1 className="myflix-title">myFlix Registration</h1>
-    <Form>
-      <Form.Group controlId="formBasicText">
-         <Form.Label>username</Form.Label>
-         <Form.Control type='text' 
-         placeholder="Enter username"       
-         value={username} 
-         pattern="^[a-zA-Z0-9]{5,}"
-         onChange={e => setUsername(e.target.value)} />
-           <Form.Control.Feedback type="invalid">
+      <Form noValidate validated={validated} onSubmit={handleSubmit} className="registration-form">
+       
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" pattern="^[a-zA-Z0-9]{5,}" required value={username} onChange={e => setUsernameAndUsed(e)} />
+          <Form.Control.Feedback type="invalid">
              Please choose a username that's at least 5 characters and is alphanumeric.
           </Form.Control.Feedback>
-          
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-             <Form.Label>Password</Form.Label>
-             <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
-             <Form.Control.Feedback type="invalid">
+          {!used ? null
+          :
+          <Form.Text className="incorrect-text">
+            Username is already being used.
+          </Form.Text>}
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" value={password} required onChange={e => setPassword(e.target.value)} />
+          <Form.Control.Feedback type="invalid">
              Please enter a password.
           </Form.Control.Feedback>
-      </Form.Group>
-
-      <Form.Group controlId='formBasicEmail'>
+        </Form.Group>
+        <Form.Group controlId="formEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Enter Email'
-          //  pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
-          />
-           <Form.Control.Feedback type="invalid">
+          <Form.Control type="email" placeholder="Enter email" pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" value={email} required onChange={e => setEmail(e.target.value)} />
+          <Form.Control.Feedback type="invalid">
              Please enter an email in the correct format.
           </Form.Control.Feedback>
-       </Form.Group>   
-     <Form.Group controlId='formBasicBirthday'>
-          <Form.Label>Birthday</Form.Label>
-          <Form.Control
-            type='date'
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-            placeholder='Enter Birthday'
-          />
-            <Form.Control.Feedback type="invalid">
+        
+        </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthdate</Form.Label>
+          <Form.Control type="date"
+           placeholder="Enter birthdate" min="1900-01-01" max={new Date().toISOString().split('T')[0]} 
+           value={birthday} required onChange={e => setBirthday(e.target.value)} />
+          <Form.Control.Feedback type="invalid">
              Please enter a birthdate.
           </Form.Control.Feedback>
         </Form.Group>
-    
-        <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button className="submit-button" variant="primary"  type="submit" onClick={handleSubmit}>Submit</Button>
- </Form>
- <Form.Group>
-  
- <Form.Group className="login-group" controlId="formLogin">
+        <Button className="submit-button" variant="primary" type="submit">Submit</Button>
+         
+        <Form.Group className="login-group" controlId="formLogin">
           <Form.Text className="text-muted">Already have an account?</Form.Text>
           <Link to={`/`}>
             <Button className="login-link">Login here</Button>
           </Link>
         </Form.Group>
-  
- </Form.Group>
- </Container>
-  );}
-/*
-  RegistrationView.propTypes = {
-    newRegistration: PropTypes.shape({
-      username: PropTypes.string.isRequired,
-      password: PropTypes.string.isRequired,
-       email:PropTypes.string.isRequired,
-      birthday: PropTypes.string.isRequired,
-    }),
-    onRegister: PropTypes.func.isRequired, 
-    
-  };*/
+      </Form>
+    </Container>
+  );
+}
