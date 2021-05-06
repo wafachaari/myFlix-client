de4dd2318cf992b6adc9fa27abc96e9e4c52d8b0
wafachaari <wafachaari30@gmail.com>
2021-05-06 15:18:00 +0200

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -55020,7 +55020,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64830" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49426" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
