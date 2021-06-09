b71636cf614c275ba5be51a801f65aece09351ab
wafachaari <wafachaari30@gmail.com>
2021-06-09 16:09:38 +0200

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -19,7 +19,8 @@ export class ProfileView extends React.Component {
     super(props);
     this.state = {
       Username: "",
-      password: "",
+      validated: null,
+      Password: "",
       userinfo: null,
       email: "",
       birthday: "",
@@ -62,27 +63,37 @@ export class ProfileView extends React.Component {
   }
 
   updateUser(e, username, password, email, birthday) {
-    e.preventDefault();
-    const url =
-      "https://movie-api-db-30.herokuapp.com/users/" +
-      localStorage.getItem("user");
-    axios
-      .put(
-        url,
-        {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday,
+   /* if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.setState({
+        validated: true,
+        show: null
+      })
+      return;
+    }*/
+    console.log("ss");
 
-        },
-        {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      )
+    e.preventDefault();
+    //  const url =`https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem("user")}`
+    console.log(email);
+    console.log(localStorage.getItem('user'));
+
+    axios({
+      method: 'put',
+      url: `https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem('user')}`,
+      headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+      data: {
+
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      }
+
+    })
       .then((response) => {
+        console.log("ll");
         localStorage.setItem("user", response.data.Username);
         // props.setUsername(data.Username);
         alert("Your profile was updated successfully");
@@ -150,7 +161,7 @@ export class ProfileView extends React.Component {
   }
   render() {
 
-    const { movies, user, movie } = this.props;
+    const { movies, user, movie,validated } = this.props;
     //  const favoriteMovieList = this.state.favoriteMovies.filter(movie =>  movie._id != id) 
     //this.setState({favoriteMovies:favoriteMovieList});
     const favoriteMovieList = movies.filter((movie) => {
@@ -164,30 +175,39 @@ export class ProfileView extends React.Component {
 
       <Container className="profile-view" fluid="true">
         <h1 className="myflix-title">Update-UserInformation</h1>
-        <Form>
+
+        <Form noValidate validated={validated} onSubmit={(e) => this.updateUser(e, this.username, this.password, this.email, this.birthday)} className="profile-form">
+
           <Form.Group controlId="formBasicText">
             <Form.Label>username</Form.Label>
             <Form.Control type='text'
               defaultValue={this.state.username}
               onChange={e => this.setUsername(e.target.value)} />
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId="formPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password"
               placeholder="Password"
-              // defaultValue={this.state.password}
-
+              defaultValue=""
+              required
+              placeholder="Enter a new password"
               onChange={e => this.setPassword(e.target.value)} />
+            <Form.Control.Feedback type="invalid">
+              Please enter a password.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email</Form.Label>
             <Form.Control
               type='email'
+              pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" required
               defaultValue={this.state.email}
               onChange={e => this.setEmail(e.target.value)}
               placeholder='Enter Email'
-            />
+            />   <Form.Control.Feedback type="invalid">
+              Please enter an email in the correct format.
+          </Form.Control.Feedback>
           </Form.Group>
           <Form.Group controlId='formBasicBirthday'>
             <Form.Label>Birthday</Form.Label>
@@ -202,7 +222,7 @@ export class ProfileView extends React.Component {
           <Form.Group controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
-          <Button className="update-button" variant="primary" type="submit" onClick={(e) => this.updateUser(e, this.username, this.password, this.email, this.birthday)} >
+          <Button className="update-button" variant="primary" type="submit">
             update</Button>
           <Button className="deregister-button" variant="primary" type="submit" onClick={(e) => this.deregisterUser(e)} > deregister </Button>
 
@@ -231,24 +251,24 @@ export class ProfileView extends React.Component {
                 </div>
               );
             })}
-        </ListGroup>
+          </ListGroup>
         </Container>
-        </Container>
+      </Container>
     );
 
   }
 }
 
 ProfileView.propTypes = {
-          user: PropTypes.shape({
-          FavoriteMovies: PropTypes.arrayOf(
+  user: PropTypes.shape({
+    FavoriteMovies: PropTypes.arrayOf(
       PropTypes.shape({
-          _id: PropTypes.string.isRequired,
+        _id: PropTypes.string.isRequired,
 
       })
     ),
     Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
+    // Password: PropTypes.string.isRequired,
     Email: PropTypes.string.isRequired,
     Birthday: PropTypes.string.isRequired
   }),
