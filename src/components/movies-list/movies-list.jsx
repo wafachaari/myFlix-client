d5d1fc4e1ddccd5b03503669983bb4b82cd4f747
wafachaari <wafachaari30@gmail.com>
2021-06-10 00:56:34 +0200

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import { connect } from 'react-redux';
-
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
 
@@ -13,13 +12,10 @@ const mapStateToProps = state => {
 function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
-
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
   }
-
   if (!movies) return <div className="main-view"/>;
-
   return <>
     <Col md={12} style={{ margin: '1em' }}>
       <VisibilityFilterInput visibilityFilter={visibilityFilter} />
@@ -31,5 +27,4 @@ function MoviesList(props) {
     ))}
   </>;
 }
-
 export default connect(mapStateToProps)(MoviesList);
