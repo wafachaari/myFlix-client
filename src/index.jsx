85114e33d71f6ce7af5435dd3022bf2e0f8515db
wafachaari <wafachaari30@gmail.com>
2021-06-09 16:09:34 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
 import './index.scss';
 import Container from 'react-bootstrap/Container';
- import { createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';
@@ -12,8 +12,8 @@ const store = createStore(moviesApp, devToolsEnhancer());
 
 class MyFlixApplication extends React.Component {
   render() {
-    return(
-     <Provider store={store}>
+    return (
+      <Provider store={store}>
         <Container>
           <MainView />
         </Container>
