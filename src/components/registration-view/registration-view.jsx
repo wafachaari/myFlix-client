71e5280b3938d579ce1b3ee0876c29a3368158f7
wafachaari <wafachaari30@gmail.com>
2021-06-10 00:56:26 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -6,14 +6,13 @@ import Container from 'react-bootstrap/Container';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-export function RegistrationView() { 
+export function RegistrationView() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
   const [validated, setValidated] = useState('');
   const [used, setUsed] = useState('');
-
   const handleSubmit = (e) => {
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
@@ -23,11 +22,9 @@ export function RegistrationView() {
       setValidated(true);
       return;
     }
+
     e.preventDefault();
-    console.log(name, username);
-    /* Send a request to the server for authentication */
     axios.post('https://movie-api-db-30.herokuapp.com/users', {
-      
       Username: username,
       Password: password,
       Email: email,
@@ -51,51 +48,46 @@ export function RegistrationView() {
     setUsername(e.target.value);
     setUsed(null);
   }
-
-
   return (
     <Container className="registration-view" fluid="true">
       <h1 className="myflix-title">myFlix Registration</h1>
       <Form noValidate validated={validated} onSubmit={handleSubmit} className="registration-form">
-       
         <Form.Group controlId="formUsername">
           <Form.Label>Username</Form.Label>
           <Form.Control type="text" placeholder="Enter username" pattern="^[a-zA-Z0-9]{5,}" required value={username} onChange={e => setUsernameAndUsed(e)} />
           <Form.Control.Feedback type="invalid">
-             Please choose a username that's at least 5 characters and is alphanumeric.
+            Please choose a username that's at least 5 characters and is alphanumeric.
           </Form.Control.Feedback>
-          {!used ? null
-          :
-          <Form.Text className="incorrect-text">
-            Username is already being used.
+          {used ? null
+            :
+            <Form.Text className="incorrect-text">
+              Username is already being used.
           </Form.Text>}
         </Form.Group>
         <Form.Group controlId="formPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Enter password" value={password} required onChange={e => setPassword(e.target.value)} />
           <Form.Control.Feedback type="invalid">
-             Please enter a password.
+            Please enter a password.
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" value={email} required onChange={e => setEmail(e.target.value)} />
           <Form.Control.Feedback type="invalid">
-             Please enter an email in the correct format.
+            Please enter an email in the correct format.
           </Form.Control.Feedback>
-        
         </Form.Group>
         <Form.Group controlId="formBirthday">
           <Form.Label>Birthdate</Form.Label>
           <Form.Control type="date"
-           placeholder="Enter birthdate" min="1900-01-01" max={new Date().toISOString().split('T')[0]} 
-           value={birthday} required onChange={e => setBirthday(e.target.value)} />
+            placeholder="Enter birthdate" min="1900-01-01" max={new Date().toISOString().split('T')[0]}
+            value={birthday} required onChange={e => setBirthday(e.target.value)} />
           <Form.Control.Feedback type="invalid">
-             Please enter a birthdate.
+            Please enter a birthdate.
           </Form.Control.Feedback>
         </Form.Group>
         <Button className="submit-button" variant="primary" type="submit">Submit</Button>
-         
         <Form.Group className="login-group" controlId="formLogin">
           <Form.Text className="text-muted">Already have an account?</Form.Text>
           <Link to={`/`}>
