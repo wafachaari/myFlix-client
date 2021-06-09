aaba942888294cb25d93785e8bf63e8a8ecfcc7a
wafachaari <wafachaari30@gmail.com>
2021-06-10 00:56:37 +0200

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
-import Card from 'react-bootstrap/Card';
 import { Form, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom'
 import axios from 'axios';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import { MovieCard } from '../movie-card/movie-card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Popover from 'react-bootstrap/Popover';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Spinner from 'react-bootstrap/Spinner';
+import './profile-view.scss'
 export class ProfileView extends React.Component {
 
   constructor(props) {
@@ -27,6 +19,7 @@ export class ProfileView extends React.Component {
       favoriteMovies: [],
       favorite: [],
       movies: "",
+      onLogOut: null,
     };
   }
 
@@ -34,14 +27,12 @@ export class ProfileView extends React.Component {
     if (date) date = date.substring(0, 10);
     return date;
   }
+
   componentDidMount() {
-    console.log(localStorage.getItem('user'));
-    console.log(localStorage.getItem('user'));
     this.getUserInfo(localStorage.getItem('user'), localStorage.getItem('token'));
   }
 
   getUserInfo(user, token) {
-    console.log(localStorage.getItem("user"));
     let url =
       "https://movie-api-db-30.herokuapp.com/users/" +
       localStorage.getItem("user");
@@ -50,137 +41,98 @@ export class ProfileView extends React.Component {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        console.log(response.data.FavoriteMovies),
-          this.setState({
-
-            username: response.data.Username,
-            password: response.data.Password,
-            email: response.data.Email,
-            birthday: this.formatDate(response.data.Birthday),
-            favoriteMovies: response.data.FavoriteMovies,
-          });
+        this.setState({
+          username: response.data.Username,
+          password: response.data.Password,
+          email: response.data.Email,
+          birthday: this.formatDate(response.data.Birthday),
+          favoriteMovies: response.data.FavoriteMovies,
+        });
       });
   }
 
   updateUser(e, username, password, email, birthday) {
-   /* if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-      this.setState({
-        validated: true,
-        show: null
-      })
-      return;
-    }*/
-    console.log("ss");
-
+    console.log(birthday);
     e.preventDefault();
-    //  const url =`https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem("user")}`
-    console.log(email);
-    console.log(localStorage.getItem('user'));
-
     axios({
       method: 'put',
       url: `https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem('user')}`,
       headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
       data: {
-
         Username: username,
         Password: password,
         Email: email,
         Birthday: birthday
       }
-
     })
       .then((response) => {
-        console.log("ll");
         localStorage.setItem("user", response.data.Username);
-        // props.setUsername(data.Username);
         alert("Your profile was updated successfully");
-        window.open("/", "_self");
       })
       .catch((e) => {
         console.log(e);
-        //   alert("Username contains non alphanumeric characters - not allowed");
       });
   }
 
-
   removeFavorite(movie) {
-
-    let URL = "https://movie-api-db-30.herokuapp.com/users/" +
-      localStorage.getItem("user") +
-      "/movies/" +
-      movie;
     axios({
       method: 'delete',
-      url: URL,
+      url: `https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem('user')}/movies/${movie}`,
       headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
     })
       .then(response => {
-        console.log(response);
-        //  this.componentDidMount();
         this.getUserInfo(localStorage.getItem('user'), localStorage.getItem('token'));
       })
       .catch(e => {
         console.error(e);
       });
   }
+
   deregisterUser(e) {
     e.preventDefault();
-    const url =
-      "https://movie-api-db-30.herokuapp.com/users/" +
-      localStorage.getItem("user");
     axios({
       method: 'delete',
-      url: url,
+      url: `https://movie-api-db-30.herokuapp.com/users/${localStorage.getItem('user')}`,
       headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
     })
       .then(response => {
-        alert(" has been deleted");
-        window.location.pathname = "/";
+        alert(" account deleted");
+        this.props.onLogOut(true);
+
       })
       .catch(error => {
         console.error(error);
       })
-
   }
+
   setUsername(input) {
-    this.username = input;
-
+    this.state.username = input;
   }
+
   setPassword(input) {
-    this.password = input;
+    this.state.password = input;
   }
 
   setEmail(input) {
-    this.email = input;
+    this.state.email = input;
   }
   setBirthday(input) {
-    this.birthday = input;
+    console.log(this.state.birthday);
+    this.state.birthday = input;
   }
   render() {
 
-    const { movies, user, movie,validated } = this.props;
-    //  const favoriteMovieList = this.state.favoriteMovies.filter(movie =>  movie._id != id) 
-    //this.setState({favoriteMovies:favoriteMovieList});
+    const { movies, user, movie, validated } = this.props;
     const favoriteMovieList = movies.filter((movie) => {
       return this.state.favoriteMovies.includes(movie._id);
     });
-
-    console.log(this.state.favoriteMovies);
-
-
     return (
-
       <Container className="profile-view" fluid="true">
         <h1 className="myflix-title">Update-UserInformation</h1>
-
-        <Form noValidate validated={validated} onSubmit={(e) => this.updateUser(e, this.username, this.password, this.email, this.birthday)} className="profile-form">
-
-          <Form.Group controlId="formBasicText">
+        <Form noValidate validated={validated} onSubmit={(e) => this.updateUser(e, this.state.username, this.state.password, this.state.email, this.state.birthday)} className="profile-form">
+          <Form.Group controlId="formBasicText"  >
             <Form.Label>username</Form.Label>
-            <Form.Control type='text'
+            <Form.Control type='text' readOnly
               defaultValue={this.state.username}
               onChange={e => this.setUsername(e.target.value)} />
           </Form.Group>
@@ -196,7 +148,6 @@ export class ProfileView extends React.Component {
               Please enter a password.
             </Form.Control.Feedback>
           </Form.Group>
-
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email</Form.Label>
             <Form.Control
@@ -214,32 +165,23 @@ export class ProfileView extends React.Component {
             <Form.Control
               type='date'
               defaultValue={this.state.birthday}
-              onChange={(e) => setBirthday(e.target.value)}
+              onChange={(e) => this.setBirthday(e.target.value)}
               placeholder='Enter Birthday'
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
           <Button className="update-button" variant="primary" type="submit">
             update</Button>
           <Button className="deregister-button" variant="primary" type="submit" onClick={(e) => this.deregisterUser(e)} > deregister </Button>
-
         </Form>
-
-
         <Container className="profile-view" fluid="true">
           <h1 className="myflix-title">Favorite Movies</h1>
           <ListGroup className="favorites-groups" variant="flush">
             {favoriteMovieList.length === 0 && <ListGroup.Item>You have no favorite movies.</ListGroup.Item>}
-
-
-
-
             {favoriteMovieList.map((movie) => {
               return (
-                <div key={movie._id}>
+                // <div key={movie._id}>
+                <ListGroup.Item key={movie.title} className="favorite-movies">
                   <div>
                     {movie.Title}
                   </div>
@@ -248,14 +190,13 @@ export class ProfileView extends React.Component {
                       Remove
                       </Button>
                   </div>
-                </div>
+                </ListGroup.Item>
               );
             })}
           </ListGroup>
         </Container>
       </Container>
     );
-
   }
 }
 
@@ -264,7 +205,6 @@ ProfileView.propTypes = {
     FavoriteMovies: PropTypes.arrayOf(
       PropTypes.shape({
         _id: PropTypes.string.isRequired,
-
       })
     ),
     Username: PropTypes.string.isRequired,
@@ -272,8 +212,6 @@ ProfileView.propTypes = {
     Email: PropTypes.string.isRequired,
     Birthday: PropTypes.string.isRequired
   }),
-
   movies: PropTypes.array.isRequired,
-
   favoriteMovies: PropTypes.array,
 }
