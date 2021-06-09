d06d02268b37ac5122823d261901b3644b89c85c
wafachaari <wafachaari30@gmail.com>
2021-06-10 00:55:42 +0200

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card'; 
+import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+
 export class DirectorView extends React.Component {
 
   constructor() {
@@ -10,32 +11,28 @@ export class DirectorView extends React.Component {
     this.state = {};
   }
 
-
   render() {
-   
-      const { director } = this.props;
+    const { director } = this.props;
     return (
-      <Card className = "director-view" style={{ width: '32rem' }}>
-       
+      <Card className="director-view" style={{ width: '32rem' }}>
         <Card.Body>
-          <Card.Title className = "director-name">{director.Name}</Card.Title>
-          <Card.Text className = "director-bio">{director.Bio}</Card.Text>
-          <Card.Title className = "director-birth">{director.Birth}</Card.Title>
-          <Card.Text className = "director-death">{director.Death}</Card.Text>
-          <Link to = {`/`}>
+          <Card.Title className="director-name">{director.Name}</Card.Title>
+          <Card.Text className="director-bio">{director.Bio}</Card.Text>
+          <Card.Title className="director-birth">{director.Birth}</Card.Title>
+          <Card.Text className="director-death">{director.Death}</Card.Text>
+          <Link to={`/`}>
             <Button className="back-button">Back</Button>
           </Link>
         </Card.Body>
       </Card>
     );
-
-
-  }}
-  DirectorView.propTypes = {
-    director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string.isRequired,
-    }).isRequired
-  };
+  }
+}
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+    Death: PropTypes.string.isRequired,
+  }).isRequired
+};
